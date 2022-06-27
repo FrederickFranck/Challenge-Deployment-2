fae6e56c857e58cce7b4d0d0d91dc76266023da3
Francky_ <frederick_spore@hotmail.com>
2022-06-27 16:47:47 +0200

    Added session states & exploration

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 import streamlit as st
-import pandas as pd
 import project.database as db
 
+# Streamlit session state init
+if "selected_game" not in st.session_state:
+    st.session_state["selected_game"] = "Null"
+    
+if "select_main" not in st.session_state:
+    st.session_state["select_main"] = 0
+    
+if "selected_genre" not in st.session_state:
+    st.session_state["selected_genre"] = 0
+
+if "selected_category" not in st.session_state:
+    st.session_state["selected_category"] = 0
+    
+
 st.set_page_config(layout="wide")
+st.title("""Welcome to the Steam Visualization App """)
+st.write("""This app is designed to visualize the data from the Steam API.""")
+st.header("Select genres or categories")
 
 _connection = db.connect()
 
+
 games = db.get_games(_connection)
-# Create a list of names of games
-names = [list(game.keys())[0] for game in games]
+st.write(games[0])
+game_ids = [list(game.values())[0] for game in games]
+st.write(game_ids[0])
+ratings = db.get_game_ratings(game_ids[0], _connection)
+supports = db.get_game_supported_platforms(game_ids[0], _connection)
+st.write(ratings)
+st.write(supports)
+
+st.session_state["select_main"] = st.selectbox("Select a category or genre", ['Genres', 'Categories'],)
 
 
-def select_game():
-    for game in games:
-        if game_name in game:
-            _game = db.get_game_by_id(game[game_name], _connection)
-
-            return st.write(_game)
-
-    return
-
-
-st.write(
-    """Hello There ! This is a Streamlit App that will help you to find the best games for you. """
-)
-
-game_name = st.selectbox("Game ? ", names, on_change=select_game)
+if st.session_state["select_main"] == "Genres":
+    genres = db.get_genres(_connection)
+    st.header("Select a genre")
+    
+    genre_names = [list(genre.keys())[0] for genre in genres]
+    st.session_state["selected_genre"] = st.selectbox("Select a genre", genre_names)
+    
+if st.session_state["select_main"] == "Categories":
+    categories = db.get_categories(_connection)
+    st.header("Select a category")
+    
+    category_names = [list(category.keys())[0] for category in categories]
+    st.session_state["selected_category"] = st.selectbox("Select a category", category_names)
